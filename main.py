b8cd1bbd13fda5452a212a110c33e86ae68b1265
abdolrahman kiany <94900618+abdoki7788@users.noreply.github.com>
2023-01-01 11:12:00 +0330

    started project and add basic files and code

---
+++ main.py
@@ -0,0 +1,24 @@
+import pygame
+from sys import exit
+
+pygame.init()
+pygame.display.set_caption('Runner')
+
+window_size = (800, 400)
+
+screen = pygame.display.set_mode(window_size)
+clock = pygame.time.Clock()
+test_surface = pygame.Surface((100, 200))
+
+test_surface.fill('Red')
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    
+    screen.blit(test_surface, (200, 100))
+    pygame.display.update()
+    clock.tick(60)
